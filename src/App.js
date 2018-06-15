906c158bb4530261ebe4dd465f485df4b5cf8997
MKAVARTS <mkavarts@gmail.com>
2018-06-15 17:50:45 -0500

    removed wrapper, using standard instance mode for p5, first audio track functional

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './elTopo.png';
 import './App.css';
-import sketch from './sketch';
-import P5Wrapper from 'react-p5-wrapper';
+import 'p5/lib/addons/p5.sound';
+import Sketchy from './newSketch';
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state={enter: false}
+  }
+  
+  pressedEnter = () => {
+    this.setState({enter: true});
+  }
+
   render() {
+    const {enter} = this.state;
+    if(!enter){
     return (
-      <div>
-        < P5Wrapper rotation={100.0} sketch={sketch}  />
+      <div className = "container align-items-center">
+        <div className="row">
+            <img alt={'el topo logo'}style={{width: 300, height: 300}} className="mx-auto" src={logo} />
+        </div>
+        <div className="row text-center">
+          <div className="col">
+            <p onClick={this.pressedEnter} className="d-inline">ENTER</p>
+          </div>
+        </div>
       </div>
     );
+  }else if(enter){
+    console.log('trying to render p5Wrapper');
+    return(
+      < Sketchy />
+    )
   }
 }
-
+}
 export default App;
