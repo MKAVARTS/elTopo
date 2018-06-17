8692e71cdbf174e95cbf23525c77e4ff34b8f66d
MKAVARTS <mkavarts@gmail.com>
2018-06-17 18:41:36 -0500

    all songs loaded, placeholder visual for each, able to select between tracks

--- src/App.js
+++ src/App.js
@@ -2,26 +2,74 @@ import React, { Component } from 'react';
 import logo from './elTopo.png';
 import './App.css';
 import 'p5/lib/addons/p5.sound';
-import Sketchy from './newSketch';
+
+import Terra from './Terra';
+import Luv from './Luv';
+import Plus from './Plus';
+import terraform from './tracks/terraform.mp3';
+import do4luv from './tracks/do4luv.mp3';
+import plusplusplus from './tracks/plusplusplus.mp3';
+
+import BackButton from './vectors/BackButton.png';
+import NextButton from './vectors/NextButton.png';
+
+let counter = 0;
 
 class App extends Component {
   
   constructor(props){
     super(props);
-    this.state={enter: false}
+    this.state={
+      enter: false,
+      playTerra: true,
+    }
   }
   
   pressedEnter = () => {
     this.setState({enter: true});
   }
 
+  nextTrack = () => {
+    console.log('trying to go to the next track');
+    counter++;
+    counter = counter % 3;
+
+    console.log(counter);
+    if(counter === 0){
+      console.log('trying to set state to play terra true');
+      this.setState({playTerra : true, playLuv : false, playPlus: false});
+    }else if(counter === 1){
+      console.log('trying to set state to play terra false');
+      this.setState({playTerra: false, playLuv: true, playPlus: false});
+    }else if(counter === 2){
+      this.setState({playTerra: false, playLuv: false, playPlus: true});
+    }
+  }
+
+  lastTrack = () => {
+    console.log('trying to go to the next track');
+    counter--;
+    counter = counter % 3;
+
+    console.log(counter);
+    if(counter <= 0){
+      counter = 0;
+      console.log('trying to set state to play terra true');
+      this.setState({playTerra : true, playLuv : false, playPlus: false});
+    }else if(counter === 1){
+      console.log('trying to set state to play terra false');
+      this.setState({playTerra: false, playLuv: true, playPlus: false});
+    }else if(counter === 2){
+      this.setState({playTerra: false, playLuv: false, playPlus: true});
+    }
+  }
+
   render() {
-    const {enter} = this.state;
-    if(!enter){
+    if(this.state.enter === false){
     return (
       <div className = "container align-items-center">
         <div className="row">
-            <img alt={'el topo logo'}style={{width: 300, height: 300}} className="mx-auto" src={logo} />
+            <img alt={'el topo logo'} style={{width: 300, height: 300}} className="mx-auto" src={logo} />
         </div>
         <div className="row text-center">
           <div className="col">
@@ -30,10 +78,51 @@ class App extends Component {
         </div>
       </div>
     );
-  }else if(enter){
-    console.log('trying to render p5Wrapper');
+
+  }else if(this.state.enter === true && this.state.playTerra === true){
     return(
-      < Sketchy />
+      <div>
+      <div>
+        < Terra />
+      </div>
+      <div className='container'>
+          <div className='row justify-content-center'>
+              <img  onClick={this.lastTrack}  alt={'back button'} className='pl-2 pr-2 mt-2 playback' src={BackButton} />
+              <audio autoPlay={true} controls={true} src={terraform} id='terraform' />
+              <img alt={'next button'} onClick={this.nextTrack} className='pl-2 pr-2 mt-2 playback' src={NextButton} />
+          </div>
+        </div>
+        </div>
+    )
+  }else if(this.state.enter === true && this.state.playLuv === true){
+    return(
+      <div>
+      <div>
+        < Luv />
+      </div>
+      <div className='container'>
+          <div className='row justify-content-center'>
+              <img onClick={this.lastTrack} alt={'back button'} className='pl-2 pr-2 mt-2 playback' src={BackButton} />
+              <audio autoPlay={true} controls={true} src={do4luv} id='terraform' />
+              <img alt={'next button'} onClick={this.nextTrack} className='pl-2 pr-2 mt-2 playback' src={NextButton} />
+          </div>
+        </div>
+        </div>
+    )
+  }else if(this.state.enter === true && this.state.playPlus === true){
+    return(
+      <div>
+      <div>
+        < Plus />
+      </div>
+      <div className='container'>
+          <div className='row justify-content-center'>
+              <img onClick={this.lastTrack} alt={'back button'} className='pl-2 pr-2 mt-2 playback' src={BackButton} />
+              <audio autoPlay={true} controls={true} src={plusplusplus} id='terraform' />
+              <img alt={'next button'} onClick={this.nextTrack} className='pl-2 pr-2 mt-2 playback' src={NextButton} />
+          </div>
+        </div>
+        </div>
     )
   }
 }
---
+++ src/Plus.js
@@ -0,0 +1,170 @@
+
+import React, { Component } from 'react';
+import p5 from 'p5';
+import 'p5/lib/addons/p5.sound.min.js';
+
+let  width = window.innerWidth;
+let  height = window.innerHeight;
+let walkerAmount = 20;
+let walkerArray = [];
+
+
+
+export default class Terra extends Component {
+
+    rootRef = node => {
+        this.root = node;
+    }
+
+    componentDidMount() {
+        new p5(this.sketch, this.root);
+        window.onresize = () => {
+          this.canvas.resize(400, window.innerHeight-150);
+        };
+    }
+
+    sketch = (p) => {
+        this.p = p;
+
+        class Particle {
+            constructor(){
+                this.location = p.createVector(p.random(100, 200),p.random(100, 200));
+                this.velocity = p.createVector(0,0);
+                this.acceleration = p.createVector(0,0);
+                this.step = 2;
+
+                this.fillColorOneFunction = () => {
+                    var pickAColor = p.floor(p.random(3));
+    
+                    switch(pickAColor){
+                        case 0: 
+                        return '#DC143C';
+                        case 1: 
+                         return '#f28500';
+                        case 2: 
+                        return '#ffffff';
+                    }
+                }
+
+                this.fillColorTwoFunction = () => {
+                    var pickAColor = p.floor(p.random(3));
+    
+                    switch(pickAColor){
+                        case 0: 
+                        return '#DC143C';
+                        case 1: 
+                         return '#f28500';
+                        case 2: 
+                        return '#ffffff';
+                    }
+                }
+
+
+                this.fillColorOne = this.fillColorOneFunction();
+                this.fillColorTwo = this.fillColorTwoFunction();
+        
+            this.startColor = () => {
+                var pickAColor = p.floor(p.random(3));
+
+                switch(pickAColor){
+                    case 0: 
+                    this.fillColorOne = '#DC143C';
+                    this.fillColorTwo = '#f28500';
+                    break;
+                    case 1: 
+                    this.fillColorOne = '#f28500';
+                    this.fillColorTwo = '#ffffff';
+                    break;
+                    case 2: 
+                    this.fillColorOne = '#ffffff';
+                    this.fillColorTwo = '#DC143C';
+                    break;
+                }
+            }
+       
+
+            this.startPosition = () => {
+
+                this.location = p.createVector(p.random(100, 200),p.random(100, 200));
+                this.velocity = p.createVector(0,0);
+                this.acceleration = p.createVector(0,0);
+                this.positionX = this.location.x;
+                this.positionY = this.location.y;
+            }
+      
+            this.update = () => {
+
+                this.mouse = p.createVector(p.mouseX,p.mouseY);
+                this.mousePosition = this.mouse.copy();
+                this.mouse.sub(this.location);
+                this.mouse.setMag(0.8);
+                this.acceleration = this.mouse.add(p.createVector(p.random(-this.step,this.step), p.random(-this.step,this.step)));
+                this.acceleration = this.mouse;
+
+                this.velocity.add(this.acceleration);
+                this.velocity.limit(1.);
+                this.location.add(this.velocity);
+               
+                this.howClose = this.mousePosition.sub(this.location);
+               
+                if(p.mouseIsPressed && this.howClose.mag() < 10.0){
+                    this.startPosition();
+                    this.startColor();
+                    return p.clear();
+            }
+                
+            }
+
+            this.display = () => {
+
+                p.strokeWeight(0);
+                p.fill(this.fillColorOne);
+                p.ellipse(this.location.x, this.location.y, 1, 1);
+                p.fill(this.fillColorTwo);
+                p.rect(this.location.x, this.location.y, 1,1);  
+
+              }
+            } 
+        } 
+
+        p.setup = () => {
+            p.pixelDensity(1);
+            p.background(0);
+
+
+
+            this.canvas = p.createCanvas(400, window.innerHeight - 150);
+            for(var i = 0; i < walkerAmount; i++){
+                walkerArray.push(new Particle());
+              }
+
+            for(var i = 0; i < walkerAmount; i++){
+                walkerArray[i].startColor();
+                walkerArray[i].startPosition();
+            }
+            
+        }
+
+        p.draw = () => {    
+            
+        for(var i = 0; i < walkerArray.length; i++){
+            walkerArray[i].update();
+            walkerArray[i].display();
+   
+        }
+    }
+}
+
+    render(){
+      return (
+    <div className='container'>
+        <div className='row align-items-center'>
+            <div className='col text-center'>
+                <p>"plusplusplus"</p>
+                <div id="cnv" ref={this.rootRef}></div>
+            </div>
+            </div>
+    </div>
+      );
+    }
+    }
